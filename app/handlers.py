a03f75b9f212c1aaab7a564c915166d4f9b96613
zhestok1 <rp3truhin@yandex.ru>
2026-07-20 21:52:13 +0400

    сделал так чтобы бот не отвечал на пост в канале

--- app/handlers.py
+++ app/handlers.py
@@ -52,7 +52,7 @@ async def i_dont_like_sticker(message: Message):
 @router.message(F.text, F.chat.type.in_({ChatType.SUPERGROUP, ChatType.GROUP, ChatType.PRIVATE}))
 async def i_can_check_your_message(message: Message):
     
-    if message.from_user.is_bot:
+    if message.from_user.is_bot and not message.reply_to_message:
         return
     
     try:
