5e6dbb4fc1dba8579f328b1254f5333fa983a710
zhestok1 <rp3truhin@yandex.ru>
2026-07-20 14:33:17 +0400

    second commit

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ if not OPENAI_API_TOKEN:
     raise ValueError('OPENAI_API_TOKEN is not found!')
 
 client = AsyncOpenAI(
-    api_key=OPENAI_API_TOKEN,  # Pollinations не требует ключа
-    base_url="https://openrouter.ai/api/v1", # ← Только базовый URL
+    api_key=OPENAI_API_TOKEN, 
+    base_url="https://openrouter.ai/api/v1", 
     timeout=60,
 )
