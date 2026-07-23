937df7d47a668fa2367d8b194ba0f7cfe3c6c216
zhestok1 <rp3truhin@yandex.ru>
2026-07-23 21:45:48 +0400

    lu8

--- main.py
+++ main.py
@@ -17,8 +17,7 @@ if not BOT_TOKEN:
 async def main() -> None:
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher() 
-    dp.include(payment_router)
-    dp.include_router(router)
+    dp.include_router(router, payment_router)
     await dp.start_polling(bot)
     
 if __name__ == '__main__':
